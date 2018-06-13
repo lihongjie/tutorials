a71763ee0c0946e609b00906b012b76b6b8e72c2
lihongjie <lhjalyw@163.com>
2018-06-14 01:06:56 +0800

    Add projects framework-core and netty

---
+++ framework-core/src/main/java/com/github/lihongjie/core/entity/GeneralEntity.java
@@ -0,0 +1,46 @@
+package com.github.lihongjie.core.entity;
+
+import java.util.Date;
+
+public class GeneralEntity {
+
+    private Date lastUpdatedStamp;
+
+    private Date lastUpdatedTxStamp;
+
+    private Date createdStamp;
+
+    private Date createdTxStamp;
+
+    public Date getLastUpdatedStamp() {
+        return lastUpdatedStamp;
+    }
+
+    public void setLastUpdatedStamp(Date lastUpdatedStamp) {
+        this.lastUpdatedStamp = lastUpdatedStamp;
+    }
+
+    public Date getLastUpdatedTxStamp() {
+        return lastUpdatedTxStamp;
+    }
+
+    public void setLastUpdatedTxStamp(Date lastUpdatedTxStamp) {
+        this.lastUpdatedTxStamp = lastUpdatedTxStamp;
+    }
+
+    public Date getCreatedStamp() {
+        return createdStamp;
+    }
+
+    public void setCreatedStamp(Date createdStamp) {
+        this.createdStamp = createdStamp;
+    }
+
+    public Date getCreatedTxStamp() {
+        return createdTxStamp;
+    }
+
+    public void setCreatedTxStamp(Date createdTxStamp) {
+        this.createdTxStamp = createdTxStamp;
+    }
+}

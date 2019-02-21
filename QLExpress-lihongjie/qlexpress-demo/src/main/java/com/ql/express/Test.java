e74d001c08e35a45f1093cd4f7a1a346a4b38b6f
lihongjie520 <lhjalyw@163.com>
2019-02-21 19:48:53 +0800

    Add some practices

---
+++ QLExpress-lihongjie/qlexpress-demo/src/main/java/com/ql/express/Test.java
@@ -0,0 +1,20 @@
+package com.ql.express;
+
+import com.ql.util.express.ExpressRunner;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        String express1 = " 1 in (2) ";
+        String express2 = " if(false){ 1} else{2} ";
+        String express3 = " round(4.34,1) ";
+        String express4 = " '哈哈'.equals('哈哈') ";
+        ExpressRunner runner = new ExpressRunner();
+
+        System.out.println("表达式计算：" + express1 + " 处理结果： " + runner.execute(express1, null, null, false, false) );
+        System.out.println("表达式计算：" + express2 + " 处理结果： " + runner.execute(express2, null, null, false, false) );
+        System.out.println("表达式计算：" + express3 + " 处理结果： " + runner.execute(express3, null, null, false, false) );
+        System.out.println("表达式计算：" + express4 + " 处理结果： " + runner.execute(express4, null, null, false, false) );
+
+    }
+}

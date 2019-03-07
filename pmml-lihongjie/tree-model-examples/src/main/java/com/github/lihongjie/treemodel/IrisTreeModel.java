338332815fb3eaadf24557c51128898b414e6f5b
lihongjie <lhjalyw@163.com>
2019-03-07 17:12:55 +0800

    Update treemodel demo

--- pmml-lihongjie/tree-model-examples/src/main/java/com/github/lihongjie/treemodel/IrisTreeModel.java
+++ pmml-lihongjie/tree-model-examples/src/main/java/com/github/lihongjie/treemodel/IrisTreeModel.java
@@ -2,61 +2,94 @@ package com.github.lihongjie.treemodel;
 
 import com.github.lihongjie.utils.FileUtils;
 import org.dmg.pmml.FieldName;
+import org.dmg.pmml.SimplePredicate;
+import org.dmg.pmml.tree.Node;
 import org.jpmml.evaluator.Evaluator;
 import org.jpmml.evaluator.FieldValue;
 import org.jpmml.evaluator.InputField;
+import org.jpmml.evaluator.tree.NodeScoreDistribution;
+import org.jpmml.evaluator.tree.TreeModelEvaluator;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class IrisTreeModel {
 
-        public static void main(String args[]) throws Exception {
-            IrisTreeModel iris = new IrisTreeModel();
-            File file = new File(IrisTreeModel.class.getClassLoader().getResource("iris_treemodel.pmml.xml").getPath());
-            Evaluator modelEvaluator = FileUtils.createPMMLfromFile(file);
-            printArgumentsOfModel(modelEvaluator);
-            List<String> dataLines = Files.readAllLines(Paths.get(IrisTreeModel.class.getClassLoader().getResource("dataset\\Iris_150.csv").toURI()));
-            System.out.println(dataLines.size());
-            for(String dataLine : dataLines) {
-                // System.out.println(dataLine); // (sepal_length,sepal_width,petal_length,petal_width,class)
-                if (dataLine.startsWith("sepal_length")) continue;
-                Map<FieldName, ?> arguments = iris.readArgumentsFromLine(dataLine, modelEvaluator);
-                System.out.println();
-            }
+    public static void main(String args[]) throws Exception {
+        IrisTreeModel iris = new IrisTreeModel();
+        File file = new File(IrisTreeModel.class.getClassLoader().getResource("iris_treemodel.pmml.xml").getPath());
+        Evaluator modelEvaluator = FileUtils.createPMMLfromFile(file);
+        printArgumentsOfModel(modelEvaluator);
+        List<String> dataLines = Files.readAllLines(Paths.get(IrisTreeModel.class.getClassLoader().getResource("dataset\\Iris_150.csv").toURI()));
+        System.out.println(dataLines.size());
+        modelEvaluator.verify();
+        for (String dataLine : dataLines) {
+            // System.out.println(dataLine); // (sepal_length,sepal_width,petal_length,petal_width,class)
+            if (dataLine.startsWith("sepal_length")) continue;
+            Map<FieldName, ?> arguments = iris.readArgumentsFromLine(dataLine, modelEvaluator);
+            Map<FieldName, ?> results = modelEvaluator.evaluate(arguments);
+            TreeModelEvaluator tree = (TreeModelEvaluator) modelEvaluator;
+            FieldName targetName = tree.getTargetFieldName();
+            Object targetValue = results.get(targetName);
+//            TreeModelEvaluator activeTree = (TreeModelEvaluator) targetValue;
+            Node activeNode = ((NodeScoreDistribution) targetValue).getNode();
+
+            TreeModelEvaluator treeModelEvaluator = (TreeModelEvaluator) modelEvaluator;
+            Node rootNode = treeModelEvaluator.getModel().getNode();
+            loopNode(rootNode, activeNode.getId());
+
+            System.out.println(results);
+        }
     }
 
-    private  Map<FieldName,?> readArgumentsFromLine(String dataLine, Evaluator modelEvaluator) {
-            Map<FieldName, FieldValue> arguments = new LinkedHashMap<>();
+    private Map<FieldName, ?> readArgumentsFromLine(String dataLine, Evaluator modelEvaluator) {
+        Map<FieldName, FieldValue> arguments = new LinkedHashMap<>();
         String[] lineArgs = dataLine.split(",");
-
-        if( lineArgs.length != 5) return arguments;
+        // 5列: sepal_width,sepal_length,petal_length,petal_width,class
+        if (lineArgs.length != 5)
+            return arguments;
+        Map<String, Object> data = new HashMap<>();
+        data.put("sepal_length", lineArgs[0]);
+        data.put("sepal_width", lineArgs[1]);
+        data.put("petal_length", lineArgs[2]);
+        data.put("petal_width", lineArgs[3]);
         List<InputField> inputFields = modelEvaluator.getInputFields();
         for (InputField inputField : inputFields) {
-              FieldName inputFieldName = inputField.getName();
-  FieldValue inputFieldValue = inputField.prepare(arguments.get(inputFieldName));
-   arguments.put(inputFieldName, inputFieldValue);
+
+            FieldName inputFieldName = inputField.getName();
+            FieldValue inputFieldValue = inputField.prepare(data.get(inputFieldName.getValue()));
+            arguments.put(inputFieldName, inputFieldValue);
         }
         return arguments;
-//        FieldValue sepalLength = modelEvaluator.(new FieldName("Sepal.Length"), lineArgs[0].isEmpty() ? 0 : lineArgs[0]);
-//        FieldValue sepalWidth = modelEvaluator.prepare(new FieldName("Sepal.Width"), lineArgs[1].isEmpty() ? 0 : lineArgs[1]);
-//        FieldValue petalLength = modelEvaluator.prepare(new FieldName("Petal.Length"), lineArgs[2].isEmpty() ? 0 : lineArgs[2]);
-//        FieldValue petalWidth = modelEvaluator.prepare(new FieldName("Petal.Width"), lineArgs[3].isEmpty() ? 0 : lineArgs[3]);
     }
 
-    public void score() {
 
-
-    }
-
-    public static void printArgumentsOfModel(Evaluator evaluator){
+    private static void printArgumentsOfModel(Evaluator evaluator) {
         System.out.println("### Active Fields of Model ####");
-        for(InputField fieldName : evaluator.getActiveFields()){
-            System.out.println("Field Name: "+ fieldName);
+        for (InputField fieldName : evaluator.getActiveFields()) {
+            System.out.println("Field Name: " + fieldName);
         }
+    }
+
+    //递归找到命中节点
+    private static void loopNode(Node node, String activeNodeId) {
+            List<Node> nodes = node.getNodes();
+            for (Node child : nodes) {
+                if (child.getId().equals(activeNodeId)) {
+                    SimplePredicate predicate = (SimplePredicate) child.getPredicate();
+                    String str = "node id is : " + child.getId() + " " + predicate.getField().getValue() + " " + predicate.getOperator().value() + " " +  predicate.getValue();
+                    System.out.println(str);
+                    break;
+                } else {
+                    if (child.hasNodes()) {
+                        loopNode(child, activeNodeId);
+                    }
+                }
+            }
     }
 }
---
+++ pmml-lihongjie/tree-model-examples/src/main/java/com/github/lihongjie/utils/OutputNode.java
@@ -0,0 +1,4 @@
+package com.github.lihongjie.utils;
+
+public class OutputNode {
+}

83e7daa0eee6c60d2783d5a110b05e039dcae59c
lihongjie <lhjalyw@163.com>
2019-06-06 08:57:33 +0800

    Add semaphore example

--- java-lihongjie/core-java-concurrency/src/main/java/com/github/lihongjie/currency/semaphore/SemaphoreExample.java
+++ java-lihongjie/core-java-concurrency/src/main/java/com/github/lihongjie/currency/semaphore/SemaphoreExample.java
@@ -12,7 +12,7 @@ public class SemaphoreExample implements Runnable {
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 1; i++) {
             executorService.execute(new SemaphoreExample());
         }
         executorService.shutdown();
--- java-lihongjie/core-java-concurrency/src/main/java/com/github/lihongjie/currency/volatileexample/Processor.java
+++ java-lihongjie/core-java-concurrency/src/main/java/com/github/lihongjie/currency/volatileexample/Processor.java
@@ -1,6 +1,7 @@
 package com.github.lihongjie.currency.volatileexample;
 
 /**
+ * 线程可见性:
  * An example of using volatile as signaling flag
  */
 public class Processor implements Runnable {
